6e1a551384edbe090f070235a6e912c688a5c9a0
Kareemrreda <106902105+Kareemrreda@users.noreply.github.com>
2025-11-26 15:17:50 +0200

    .

--- Preprocessing_Landmarks/preprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Preprocessing package (v3 global-mean root+scale + safehands)."""
-
-from .pipeline_v3 import preprocess_sequence_global
-
-__all__ = ["preprocess_sequence_global"]
--- Preprocessing_Landmarks/scripts/preprocess_v3.py
+++
@@ -1,4 +0,0 @@
-from preprocessing.cli_v3 import main
-
-if __name__ == "__main__":
-    main()
